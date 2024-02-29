fbaa85e1825fedbde6f2900325b261676c840b99
Louie Pascual <louieepascual@gmail.com>
2024-02-29 14:32:53 +0800

    add arch and image_name args for docker build sript
    Signed-off-by: Louie Pascual <louieepascual@gmail.com>

--- build-docker.sh
+++ build-docker.sh
@@ -10,15 +10,17 @@ fi
 TAGS=()
 UPDATE=0
 VERSION=
-ARCH="amd64"
+ARCH=
+IMAGE_NAME="chromedp/headless-shell"
 
 OPTIND=1
-while getopts "t:uv:p:" opt; do
+while getopts "t:uv:p:i:" opt; do
 case "$opt" in
   t) TAGS+=($OPTARG) ;;
   u) UPDATE=1 ;;
   v) VERSION=$OPTARG ;;
   p) ARCH=$OPTARG ;;
+  i) IMAGE_NAME=$OPTARG ;;
 esac
 done
 
@@ -26,6 +28,13 @@ if [ -z "$VERSION" ]; then
   pushd $SRC/out &> /dev/null
   VERSION=$(ls *.bz2|sort -r -V|head -1|sed -e 's/^headless-shell-//' -e 's/-.*\.tar\.bz2$//')
   popd &> /dev/null
+fi
+
+if [ -z "$ARCH" ]; then
+    ARCH="$(uname -m)"
+    if [ "$ARCH" = "x86_64" ]; then
+        ARCH="amd64"
+    fi
 fi
 
 set -e
@@ -45,9 +54,9 @@ if [ "$UPDATE" = "1" ]; then
   )
 fi
 
-PARAMS=(--tag louiepascual/headless-shell:$VERSION)
+PARAMS=(--tag $IMAGE_NAME:$VERSION)
 for TAG in ${TAGS[@]}; do
-  PARAMS+=(--tag louiepascual/headless-shell:$TAG)
+  PARAMS+=(--tag $IMAGE_NAME:$TAG)
 done
 
 (set -x;
